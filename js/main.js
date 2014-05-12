bf5e4f7524c5fc840622665dcfe3f58c258ad772
Niklas Hultström <niklashulstrom@gmail.com>
2014-05-12 23:09:12 +0200

    added name function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
+if ( window.location.hash.length > 0 ) {
+	var urlHash = decodeURI(window.location.hash).substring(1, window.location.hash.length).split('----'),
+		string = urlHash[0],
+		name = urlHash[1];
+
+	$('#firstScram').fadeOut(0, function() {
+		$('#scrambledText').text(string);
+		$('#inputName').val(name);
+		$('#main').addClass('blur');
+		$('#secondScram').fadeIn(0);
+	});
+}
+
 $('#scramble').on( "click", function() {
-	var string = $('#inputText').val();
+	var string = somethingClever($('#inputText').val()),
+		name = $('#inputName').val();
 
 	$('#firstScram').fadeOut(1000, function() {
-		$('#scrambledText').text(somethingClever(string));
+		$('#scrambledText').text(string);
 		$('#main').addClass('blur');
 		$('#secondScram').fadeIn(1000);
 	});
 
-	var urlHash = encodeURI(string);
-	window.ph_string = urlHash;
-	window.location.hash = urlHash;	
+	setUrlHash(string, name)
 });
+
+$('#inputName').on('keyup', function() {
+	var string = somethingClever($('#inputText').val()),
+		name = $('#inputName').val();
+
+	setUrlHash(string, name)
+});
+
+function setUrlHash(string, name) {
+	var urlHash = encodeURI(string + '----' + name);
+	window.ph_urlHash = urlHash;
+	window.location.hash = urlHash;	
+} 
 
 $('#inputText').focus(function() {
 	$(this).val('');
@@ -20,12 +45,12 @@ $('#inputText').focus(function() {
 	}
 });
 
-$(document).on('click', '#share-button', function() {
+$(document).on('click', '#share-button', function() {	
     var shareData = {
         name: 'Pretty Handsome Scramble',
         caption: '',
         description: 'In the world of Pretty Handsome Nerds things are often upside down, but always make sense. That is why we created a Pretty Handsome Scramble - an ultimate tool for future communication that‚ we hope, will become a language one day. A Pretty Handsome Language, that is.',
-        link: 'http://prettyhandsomenerds.github.io#' + window.ph_string,
+        link: 'http://prettyhandsomenerds.github.io#' + window.ph_urlHash,
         picture: 'http://prettyhandsomenerds.github.io/img/book.png'
     } 
     fbShare(shareData);
